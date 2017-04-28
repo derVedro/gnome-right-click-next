b62dfa6ca42a99085384e548c43600682917fe53
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-04-28 22:31:15 +0200

    files into zip root

--- make_zip.sh
+++ make_zip.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 glib-compile-schemas rcd\@criztovyl.space/schemas
 rm rcd\@criztovyl.space.zip 
-zip rcd\@criztovyl.space.zip rcd@criztovyl.space -r
+pushd >/dev/null rcd@criztovyl.space
+zip ../rcd\@criztovyl.space.zip . -r
+popd >/dev/null
