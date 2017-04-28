32b4190ac3359d8f20b3ce3ea09f5aeec489bc10
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-04-28 21:16:05 +0200

    Initial

---
+++ extension.js
@@ -0,0 +1,70 @@
+
+const Main = imports.ui.main;
+const WindowClone = imports.ui.workspace.WindowClone;
+
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
+const Prefs = Me.imports.prefs;
+
+let winInject = {}, settings;
+
+// gnome-shell-extensions/extensions/windowsNaviagator/extension.js injectToFunction
+function injectToFunction(parent, name, func) {
+    let origin = parent[name];
+    parent[name] = function() {
+        let ret;
+        ret = func.apply(this, arguments);
+        if (ret === undefined){
+            log("origin");
+            ret = origin.apply(this, arguments);
+        }
+        return ret;
+    }
+    return origin;
+}
+
+// gnome-shell-extensions/extensions/windowsNaviagator/extension.js
+function removeInjection(object, injection, name) {
+    if (injection[name] === undefined)
+        delete object[name];
+    else
+        object[name] = injection[name];
+}
+
+function init() { 
+    settings = Convenience.getSettings();
+}
+
+function enable() {
+
+    log('Enabeling rcd');
+
+    winInject['_onClicked'] = injectToFunction(WindowClone.prototype, '_onClicked', function(action, actor){
+        if(action.get_button() == 3){ // cycle
+            // right click
+            let mWin = this.realWindow.get_meta_window(),
+                workspaceNr = mWin.get_workspace().index() + 1;
+
+            log("rcd","nr", workspaceNr, "n_workspaces", global.screen.n_workspaces);
+
+            if(workspaceNr == global.screen.n_workspaces)
+                workspaceNr = 0;
+
+            mWin.change_workspace_by_index(workspaceNr, false);
+
+            if(settings.get_boolean(Prefs.SETTING_OVERLAY_CLOSE)){
+                this._selected = true
+                Main.overview.toggle();
+            }
+
+        }
+    });
+
+    log('Enabeled rcd');
+}
+
+function disable() {
+    log('Disabeling rcd')
+    removeInjection(WindowClone.prototype, winInject, '_onClicked');
+    log('Disabeled rcd')
+}
---
+++ prefs.js
@@ -0,0 +1,48 @@
+/* -*- mode: js2; js2-basic-offset: 4; indent-tabs-mode: nil -*- */
+
+const Gio = imports.gi.Gio;
+const Gtk = imports.gi.Gtk;
+const GObject = imports.gi.GObject;
+const Lang = imports.lang;
+
+//const Gettext = imports.gettext.domain('right-click-down');
+//const _ = Gettext.gettext;
+const N_ = function(e) { return e };
+const _ = N_;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
+
+const SETTING_OVERLAY_CLOSE = 'overlay-close';
+
+const RightClickDownSettingsWidget = new GObject.Class({
+    Name: 'RightClickDown.Prefs.RightClickDownSettingsWidget',
+    GTypeName: 'RightClickDownSettingsWidget',
+    Extends: Gtk.Grid,
+
+    _init : function(params) {
+        this.parent(params);
+        this.margin = 24;
+        this.row_spacing = 6;
+        this.orientation = Gtk.Orientation.VERTICAL;
+
+        this._settings = Convenience.getSettings();
+
+
+        let cbOverlay = new Gtk.CheckButton({ label: _('Close Overlay after click'), margin_top: 6 });
+        this._settings.bind(SETTING_OVERLAY_CLOSE, cbOverlay, 'active', Gio.SettingsBindFlags.DEFAULT);
+        this.add(cbOverlay);
+    },
+});
+
+function init() {
+    //Convenience.initTranslations();
+}
+
+function buildPrefsWidget() {
+    let widget = new RightClickDownSettingsWidget();
+    widget.show_all();
+
+    return widget;
+}
