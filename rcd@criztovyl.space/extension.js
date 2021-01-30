0c7cd69de3fc400fb5b6f2bbf8f376ae7134da00
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2021-01-30 21:53:37 +0100

    Gnome 3.37+ compatibility

--- rcd@criztovyl.space/extension.js
+++ rcd@criztovyl.space/extension.js
@@ -1,12 +1,14 @@
 // Send windows one workspace down by rightclick
-// (C) 2017 Christoph "criztovyl" Schulz
+// (C) 2017, 2020, 2021 Christoph "criztovyl" Schulz
 // GPLv3 and later
 const Main = imports.ui.main;
-const WindowClone = imports.ui.workspace.WindowClone;
+const { WindowPreview } = imports.ui.windowPreview;
 
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Convenience = Me.imports.convenience;
 const Prefs = Me.imports.prefs;
+
+const { GObject } = imports.gi;
 
 let winInject = {}, settings;
 
@@ -19,6 +21,17 @@ function injectToFunction(parent, name, func) {
         if (ret === undefined){
             ret = origin.apply(this, arguments);
         }
+        return ret;
+    }
+    return origin;
+}
+
+function appendToFunction(parent, name, func) {
+    let origin = parent[name];
+    parent[name] = function() {
+        let ret;
+        ret = origin.apply(this, arguments);
+        func.apply(this, arguments);
         return ret;
     }
     return origin;
@@ -40,34 +53,54 @@ function enable() {
 
     log('Enabeling rcd');
 
-    winInject['_onClicked'] = injectToFunction(WindowClone.prototype, '_onClicked', function(action, actor){
-        if(action.get_button() == 3){ // right click
+    winInject['_init'] = appendToFunction(WindowPreview.prototype, "_init", function(){
 
-            let mWin = this.realWindow.get_meta_window(),
-                workspaceNr = mWin.get_workspace().index() + 1,
-                n_workspaces = global.workspace_manager.n_workspaces;
+        // clickAction = Main.overview.viewSelector._workspacesDisplay._workspacesViews[1]._workspaces[0]._windows[0].get_actions()[0]
 
-            log("rcd","nr", workspaceNr, "n_workspaces", n_workspaces);
+        let clickAction = this.get_actions().find(a => GObject.type_name(a) == "ClutterClickAction")
 
-            // cycle
-            if(workspaceNr == n_workspaces) // cycle
-                workspaceNr = 0;
+        let clickHandlerId = GObject.signal_handler_find(clickAction, {signalId: "clicked"})
+        clickAction.disconnect(clickHandlerId)
 
-            mWin.change_workspace_by_index(workspaceNr, false);
+        clickAction.connect("clicked", function(action, actor){
 
-            if(settings.get_boolean(Prefs.SETTING_OVERLAY_CLOSE)){
-                this._selected = true; // cancel d'n'd event
-                Main.overview.toggle();
+            log("rcd", "clicked", this)
+
+            if(action.get_button() == 3){ // right click
+
+                let mWin = this._windowActor.get_meta_window(),
+                    workspaceNr = mWin.get_workspace().index() + 1,
+                    n_workspaces = global.workspace_manager.n_workspaces;
+
+                log("rcd", "nr", workspaceNr, "n_workspaces", n_workspaces);
+
+                // cycle
+                if(workspaceNr == n_workspaces) // cycle
+                    workspaceNr = 0;
+
+                mWin.change_workspace_by_index(workspaceNr, false);
+
+                if(settings.get_boolean(Prefs.SETTING_OVERLAY_CLOSE)){
+                    this._selected = true; // cancel d'n'd event
+                    Main.overview.toggle();
+                }
+
+            } else {
+                if(this._activate != undefined){
+                    this._activate()
+                } else {
+                    this._onClicked(action, actor)
+                }
             }
+        }.bind(this))
 
-        }
-    });
+    })
 
     log('Enabeled rcd');
 }
 
 function disable() {
     log('Disabeling rcd')
-    removeInjection(WindowClone.prototype, winInject, '_onClicked');
+    removeInjection(WindowPreview.prototype, winInject, '_init');
     log('Disabeled rcd')
 }
