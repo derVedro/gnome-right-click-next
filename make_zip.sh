f26ad37b39b81f7e9007e78bc629323d6c2e2371
derVedro <dervedro@googlemail.com>
2024-11-30 12:19:41 +0100

    rewrite make_zip.sh

--- make_zip.sh
+++ make_zip.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-glib-compile-schemas rcd\@criztovyl.space/schemas
-rm rcd\@criztovyl.space.zip 
-pushd >/dev/null rcd@criztovyl.space
-zip ../rcd\@criztovyl.space.zip . -r
-popd >/dev/null
+
+# creates zip with extension in working directory
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+EXTENSION_NAME="right-click-next@derVedro"
+OLD_PWD="$PWD"
+
+rm "$EXTENSION_NAME".zip 2>/dev/null
+cd "$SCRIPT_DIR"
+zip "$OLD_PWD/$EXTENSION_NAME".zip "$EXTENSION_NAME"/metadata.json "$EXTENSION_NAME"/extension.js
+
