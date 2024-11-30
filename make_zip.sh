7d53611b64b00ebda708d0b52976975857dbf150
derVedro <dervedro@googlemail.com>
2024-11-30 12:19:41 +0100

    rewrite make_zip.sh

--- make_zip.sh
+++ make_zip.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-# creates zip with extension in working directory
+# creates extension's zip in working directory
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 EXTENSION_NAME="right-click-next@derVedro"
 OLD_PWD="$PWD"
 
 rm "$EXTENSION_NAME".zip 2>/dev/null
-cd "$SCRIPT_DIR"
-zip "$OLD_PWD/$EXTENSION_NAME".zip "$EXTENSION_NAME"/metadata.json "$EXTENSION_NAME"/extension.js
-
+cd "$SCRIPT_DIR/$EXTENSION_NAME"
+zip "$OLD_PWD/$EXTENSION_NAME".zip metadata.json extension.js
